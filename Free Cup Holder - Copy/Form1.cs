156a0963d0ec8ba25bb4c1a55da28574b6fed71f
robloxboy100058 <83027818+robloxboy100058@users.noreply.github.com>
2023-06-26 21:11:59 -0500

    Add files via upload

---
+++ Free Cup Holder - Copy/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace Free_Cup_Holder
+{
+    public partial class Form1 : Form
+    {
+        // Import the necessary functions from the Windows API
+        [DllImport("winmm.dll", EntryPoint = "mciSendStringA", CharSet = CharSet.Ansi)]
+        protected static extern int mciSendString(string lpstrCommand, string lpstrReturnString, int uReturnLength, IntPtr hwndCallback);
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Send the open command to the DVD drive
+            mciSendString("set CDAudio door open", null, 0, IntPtr.Zero);
+        }
+    }
+}
